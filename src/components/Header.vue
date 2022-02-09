16422ce523f336e5a0da511fda26df1aa0e4f9c0
GuilhermeChaves1 <guilhermechaves11@gmail.com>
2022-02-08 21:10:59 -0300

    Adicionando Cabeçalho e navegação

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,35 @@
 <template>
-  <h1>Cabeçalho</h1>
+    <v-toolbar app>
+        <vtoolbar-title class="headline text-uppercase mr-4">
+            <span>Stock</span>
+            <span class="font-weight-light">Treader</span>
+        </vtoolbar-title>
+        <v-toolbar-items>
+            <v-btn flat to="/">Início</v-btn>
+            <v-btn flat to="/portfolio">Portfólio</v-btn>
+            <v-btn flat to="/stocks">Ação</v-btn>
+        </v-toolbar-items>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+            <v-btn flat>Finalizar Dia</v-btn>
+            <v-menu offset-y>
+                <v-btn flat slot="activator">Salvar & Carregar</v-btn>
+                <v-list>
+                    <v-list-tile>
+                        <v-list-tile-title>Salvar Dados</v-list-tile-title>
+                    </v-list-tile>
+                    <v-list-tile>
+                        <v-list-tile-title>Carregar Dados</v-list-tile-title>
+                    </v-list-tile>
+                </v-list>
+            </v-menu>
+        </v-toolbar-items>
+    </v-toolbar>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
