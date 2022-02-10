b88baa435e029642571f1a84c962b3dc52d80e4d
GuilhermeChaves1 <guilhermechaves11@gmail.com>
2022-02-10 20:55:02 -0300

    Configurando Axios & Firebase

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 
-
+import './plugins/axios'
 
 import router from './router'
 import store from './store/store'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false 
 
 Vue.filter('currency', value => {
 	return 'R$ ' + value.toLocaleString()
