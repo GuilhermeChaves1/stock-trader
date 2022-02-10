6c969bc087278d0547f837fc76698950dd851752
GuilhermeChaves1 <guilhermechaves11@gmail.com>
2022-02-10 20:38:44 -0300

    Adicionando Validações

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -2,29 +2,17 @@
     <v-flex class="pr-3 pb-3" xs12 md6 lg4>
         <v-card class="green darken-3 white--text">
             <v-card-title class="headline">
-                <strong
-                    >{{ stock.name }}
-                    <small>(Preço: {{ stock.price | currency }})</small></strong
-                >
+                <strong>{{ stock.name }} <small>(Preço: {{ stock.price | currency }})</small></strong>
             </v-card-title>
         </v-card>
         <v-card>
             <v-container fill-height>
-                <v-text-field
-                    label="Quantidade"
-                    type="number"
-                    v-model.number="quantity"
-                />
-                <v-btn
-                    class="green darken-3 white--text"
-                    :disabled="
-                        insufficientFunds ||
-                        quantity <= 0 ||
-                        !Number.isInteger(quantity)
-                    "
-                    @click="buyStock"
-                    >Comprar</v-btn
-                >
+                <v-text-field label="Quantidade" type="number"
+                    :error="insufficientFunds || !Number.isInteger(quantity)"
+                    v-model.number="quantity" />
+                <v-btn class="green darken-3 white--text"
+                    :disabled="insufficientFunds || quantity <= 0 || !Number.isInteger(quantity)"
+                    @click="buyStock">{{ insufficientFunds ? 'Insuficiente' : 'Comprar' }}</v-btn>
             </v-container>
         </v-card>
     </v-flex>
@@ -32,26 +20,34 @@
 
 <script>
 export default {
-    props: ["stock"],
+    props: ['stock'],
     data() {
         return {
-            quantity: 0,
-        };
+            quantity: 0
+        }
     },
-
+    computed: {
+        funds() {
+            return this.$store.getters.funds
+        },
+        insufficientFunds() {
+            return this.quantity * this.stock.price > this.funds
+        }
+    },
     methods: {
         buyStock() {
             const order = {
                 stockId: this.stock.id,
                 stockPrice: this.stock.price,
-                quantity: this.quantity,
-            };
-            this.$store.dispatch("buyStock", order);
-            this.quantity = 0;
-        },
-    },
-};
+                quantity: this.quantity
+            }
+            this.$store.dispatch('buyStock', order)
+            this.quantity = 0
+        }
+    }
+}
 </script>
 
 <style>
+
 </style>
